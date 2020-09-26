08eb2dd7d3d39d965154ccb1ed1d2370705c58a4
unknown <andrewmacatangay5@gmail.com>
2020-09-26 13:39:04 -0500

    Added 2D

--- Code/ProjectCode/src/main/java/DnD.java
+++ Code/ProjectCode/src/main/java/DnD.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class DnD
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        GameBoard gameBoard = new GameBoard();
+        Scanner input = new Scanner(System.in);
+
+        while (true)
+        {
+            gameBoard.printBoard();
+            gameBoard.doPlayerMove(input.next());
+        }
     }
 }
